af0651379ddf54cb6a3461517afaeb442df44432
ElectroNeutrino <42867025+ElectroNeutrino@users.noreply.github.com>
2018-12-26 12:02:17 -0600

    Update multiprocess_test.py
    Added second queue to demonstrate multi-value output

--- multiprocess_test.py
+++ multiprocess_test.py
@@ -3,7 +3,7 @@ import multiprocessing
 from multiprocessing import SimpleQueue
 
 
-def mp_func_test(ra, wa_shape, tv, lock, mp_queue):
+def mp_func_test(ra, wa_shape, tv, lock, queue, queue2):
     wa = numpy.zeros(wa_shape[0] * wa_shape[1])
     wa = wa.reshape(wa_shape)
     with lock:
@@ -13,7 +13,8 @@ def mp_func_test(ra, wa_shape, tv, lock, mp_queue):
                 wa[x][y] = -ra[x][y]
                 tv.value = 5
         wa[1][1] = 100
-        mp_queue.put(wa)
+        queue.put(wa)
+        queue2.put(tv.value)
 
     return
                 
@@ -21,14 +22,15 @@ def mp_func_test(ra, wa_shape, tv, lock, mp_queue):
 
 if __name__ == '__main__':
 
-    shape = (4, 4)
+    shape = (10, 4)
     write_lock = multiprocessing.Lock()
     mp_queue = SimpleQueue()
+    mp2_queue = SimpleQueue()
     read_array = numpy.random.rand(shape[0], shape[1]) # Create an array
     write_array = numpy.zeros(shape)
     
     test_value = multiprocessing.Value('d', 10) # create multiprocess scalar
-    my_args=(read_array, shape, test_value, write_lock, mp_queue)
+    my_args=(read_array, shape, test_value, write_lock, mp_queue, mp2_queue)
     p = multiprocessing.Process(target=mp_func_test, args=my_args)
 
     p.start()
@@ -36,6 +38,9 @@ if __name__ == '__main__':
 
     if not mp_queue.empty():
         write_array = mp_queue.get()
+
+    if not mp2_queue.empty():
+        queue_value = mp2_queue.get()
 
     print("read_array")
     print(read_array[:])
@@ -45,3 +50,6 @@ if __name__ == '__main__':
 
     print("test_value")
     print(test_value.value)
+
+    print("queue_value")
+    print(queue_value)
